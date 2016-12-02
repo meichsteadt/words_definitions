0c236b14f4c2243a442ea8096a29d8322e994757
Matt Eichsteadt <matteichsteadt@yahoo.com>
2016-12-02 11:13:06 -0800

    inital commit

---
+++ lib/template.rb
@@ -0,0 +1,5 @@
+class Class
+  def method_name
+
+  end
+end
---
+++ spec/template_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'pry'
+require 'template.rb'
+
+describe 'Class' do
+  descrbe 'method' do
+    it 'will do something' do
+      expect("input".method.to eq('output')
+    end
+  end
+end
